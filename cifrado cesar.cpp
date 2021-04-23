5ee0791c2d3a04dcb5dfacf085903cca000c0658
Pablito290 <70559782+Pablito290@users.noreply.github.com>
2021-04-22 20:20:10 -0500

    Create cifrado cesar.cpp

---
+++ cifrado cesar.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+using namespace std;
+const int clave=3;	   
+const string abecedario="abcdefghijklmnopqrstuvwxyz";
+
+
+class AlgoritmoX{/*
+	private:
+		int clave=3;	   //no estoy seguro si el lugar correcto donde poner esto es acá o en una variable global asi que lo dejo como comentario en un caso esta sea el lugar
+		string abecedario="abcdefghijklmnopqrstuvwxyz";
+*/
+	public:	
+	string cifrado(string mensaje,int clave=3){
+		string cifrado;
+		for(int i=0;i<mensaje.length();i++){
+			int aux=0;
+			aux=abecedario.find(mensaje[i]);
+			aux=aux+clave%26;
+			if(aux==clave-1){
+				cifrado.append(1,' ');}
+			else{
+			cifrado.append(1,abecedario[aux]);
+		}
+}
+		return cifrado;	
+	}
+	
+	string descifrado(string mensaje,int clave=3){
+		string cifrado;
+		for(int i=0;i<mensaje.length();i++){
+			int aux=0;
+			aux=abecedario.find(mensaje[i]);
+			aux=aux-clave%26;
+			if(aux==0-(clave+1)){
+				cifrado.append(1,' ');}
+			else{
+			
+			cifrado.append(1,abecedario[aux]);
+		}
+}
+		return cifrado;	
+	}
+};
+
+string cifradoopcional(string mensaje,int clave=3){	//esta funcion la hago para hacer una funcion fuerza bruta y averiguar la clave
+		string cifrado;
+		for(int i=0;i<mensaje.length();i++){
+			int aux=0;
+			aux=abecedario.find(mensaje[i]);
+			aux=aux+clave%26;
+			if(aux==clave-1){
+				cifrado.append(1,' ');}
+			else{
+			cifrado.append(1,abecedario[aux]);
+		}
+}
+		return cifrado;	
+	}
+	
+	
+int Fuerza_Bruta(string mensaje,string pista){ //lo que hace esta funcion es pedir el mensaje original y el mensaje final
+	if(mensaje.size()==pista.size()){
+	int posible=0;
+	string aux;
+	bool confirmacion=false;
+	while(confirmacion==false){
+		aux=cifradoopcional(mensaje,posible);
+		if(aux[0]==pista[0]){
+			confirmacion=true; 
+		}
+		else{
+		posible++;
+		} }
+	return posible;
+}
+	else{
+		cout<<endl<<"Los valores ingresados no coinciden por lo cual revise su codigo"<<endl;
+		return -1;
+	}
+}
+
+int main(){
+	string mensaje="awita de coco";
+	cout<<"Mensaje Original:"<<mensaje<<endl;
+	AlgoritmoX Emisor;
+	AlgoritmoX Receptor;
+	string msj_cifrado=Emisor.cifrado(mensaje);
+	string msj_descifrado=Receptor.descifrado(msj_cifrado);
+	cout<<"Cifrado:"<<msj_cifrado;
+	cout<<endl<<"Descifrado:"<<msj_descifrado;
+	int la_clave=Fuerza_Bruta("awita de coco","dzlwd gh frfr");
+	cout<<endl<<"La clave es:"<<la_clave;
+
+}
